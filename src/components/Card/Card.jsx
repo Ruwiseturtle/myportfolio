255d5c115e56e0d950c0fc66431bc7d70e1057a2
Matviienko Ruslana <brauniesrus@gmail.com>
2024-08-20 10:22:56 +0300

    href for projects

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import './Card.css';
+import imageHref from '../../assets/images/href.png';
 
 const Card = ({ project }) => {
   return (
     <li>
       <div className="card">
-        <img className="image-project"  src={project.image} alt={project.title} />
+        <img
+          className="image-project"
+          src={project.image}
+          alt={project.title}
+        />
         <p className="title-project">{project.title}</p>
+        <a
+          href="https://www.instagram.com/vychovanetcruslana/"
+          target="_blank"
+          rel="noreferrer"
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          <div className='box-href-to-project'>
+            <img className='image-href'
+              src={imageHref}
+              alt="Instagram logo"
+            />
+            <span>Follow me on Instagram</span>
+          </div>
+        </a>
       </div>
     </li>
   );
