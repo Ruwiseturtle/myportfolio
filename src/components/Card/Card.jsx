6da1c77181af881febbfc0d425e3f43faf08135f
Matviienko Ruslana <brauniesrus@gmail.com>
2024-08-20 15:25:20 +0300

    project images

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
-import './Card.css';
-import imageHref from '../../assets/images/href.png';
+import React from "react";
+import "./Card.css";
+import imageHref from "../../assets/images/href.png";
+
+// Якщо ваші зображення знаходяться в папці `src/assets/images/`
+const getImagePath = (imageName) => {
+  try {
+    return require(`../../assets/projects/${imageName}`);
+  } catch (err) {
+    console.error(`Image ${imageName} not found`);
+    return null;
+  }
+};
+
+
 
 const Card = ({ project }) => {
+const imageSrc = getImagePath(project.image);
+ 
   return (
     <li>
       <div className="card">
-        <img
+        {/* <img
           className="image-project"
           src={project.image}
           alt={project.title}
-        />
+        /> */}
+        {imageSrc ? (
+          <img className="image-project" src={imageSrc} alt={project.title} />
+        ) : (
+          <p>Image not found</p>
+        )}
+        
         <p className="text-framework">{project.frameworks}</p>
         <a
           href="https://www.instagram.com/vychovanetcruslana/"
@@ -20,7 +40,7 @@ const Card = ({ project }) => {
         >
           <div className="box-href-to-project">
             <img className="image-href" src={imageHref} alt="Instagram logo" />
-            <span className='title-text-card'>{project.title}</span>
+            <span className="title-text-card">{project.title}</span>
           </div>
         </a>
       </div>
@@ -28,4 +48,4 @@ const Card = ({ project }) => {
   );
 };
 
-export default Card
+export default Card;
