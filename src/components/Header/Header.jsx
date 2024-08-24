4d6e1cbe030f5b44c5ac7864106678f6f251e5f7
Matviienko Ruslana <brauniesrus@gmail.com>
2024-08-24 15:52:05 +0300

    fixed homepage

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,26 +8,48 @@ const Header = () => {
     <>
       <header className="container-header">
         <div className="myLogo">
-          <NavLink className="text" to="/">
+          <NavLink
+            className={({ isActive }) => (isActive ? "text active" : "text")}
+            to="/"
+          >
             Ruslana
           </NavLink>
         </div>
 
         <div className="box-links">
-          <NavLink className="text" to="/AboutMe">
+          <NavLink
+            className={({ isActive }) => (isActive ? "text active" : "text")}
+            to="/AboutMe"
+          >
             About me
           </NavLink>
 
-          <NavLink className="text" to="/MyProjects">
+          <NavLink
+            className={({ isActive }) => (isActive ? "text active" : "text")}
+            to="/MyProjects"
+          >
             Projects
           </NavLink>
 
-          <NavLink className="text" to="/Skills">
+          <NavLink
+            className={({ isActive }) => (isActive ? "text active" : "text")}
+            to="/Skills"
+          >
             My skills
           </NavLink>
 
-          <NavLink className="text" to="/Contact">
+          <NavLink
+            className={({ isActive }) => (isActive ? "text active" : "text")}
+            to="/Contact"
+          >
             Contact
+          </NavLink>
+
+          <NavLink
+            className={({ isActive }) => (isActive ? "text active" : "text")}
+            to="/Sertificates"
+          >
+            Sertificates
           </NavLink>
         </div>
       </header>
