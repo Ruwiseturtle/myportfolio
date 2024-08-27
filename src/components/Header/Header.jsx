4800ca88b25f7c5bca1ece584ce4b31126216a8a
Matviienko Ruslana <brauniesrus@gmail.com>
2024-08-27 16:24:57 +0300

    adaptation of the certificates page for 960px

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Header.css";
 import { NavLink } from "react-router-dom";
 import logoImage from "../../assets/images/logo-code1.png";
 
 const Header = () => { 
+  const [isOpen, setIsOpen] = useState(false);
+  
+    const toggleMenu = () => {
+      setIsOpen(!isOpen);
+  };
 
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+  
   return (
     <>
       <header className="container-header">
@@ -13,15 +22,17 @@ const Header = () => {
           <NavLink
             className={({ isActive }) => (isActive ? "text active" : "text")}
             to="/"
+            onClick={closeMenu}
           >
             Ruslana
           </NavLink>
         </div>
 
-        <div className="box-links">
+        <div className={`box-links ${isOpen ? "open" : ""}`}>
           <NavLink
             className={({ isActive }) => (isActive ? "text active" : "text")}
             to="/AboutMe"
+            onClick={closeMenu}
           >
             About me
           </NavLink>
@@ -29,6 +40,7 @@ const Header = () => {
           <NavLink
             className={({ isActive }) => (isActive ? "text active" : "text")}
             to="/MyProjects"
+            onClick={closeMenu}
           >
             Projects
           </NavLink>
@@ -36,6 +48,7 @@ const Header = () => {
           <NavLink
             className={({ isActive }) => (isActive ? "text active" : "text")}
             to="/Skills"
+            onClick={closeMenu}
           >
             My skills
           </NavLink>
@@ -43,6 +56,7 @@ const Header = () => {
           <NavLink
             className={({ isActive }) => (isActive ? "text active" : "text")}
             to="/Contact"
+            onClick={closeMenu}
           >
             Contact
           </NavLink>
@@ -50,10 +64,21 @@ const Header = () => {
           <NavLink
             className={({ isActive }) => (isActive ? "text active" : "text")}
             to="/Sertificates"
+            onClick={closeMenu}
           >
             Sertificates
           </NavLink>
         </div>
+
+        {/* бергер-меню */}
+        <div className="burger" onClick={toggleMenu}>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
+
+        {/* backdrop */}
+        {isOpen && <div className="backdrop" onClick={closeMenu}></div>}
       </header>
     </>
   );
