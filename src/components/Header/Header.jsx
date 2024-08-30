944b19eb1efb301e74aa3ed6ba0683c970ea8423
Matviienko Ruslana <brauniesrus@gmail.com>
2024-08-30 11:21:07 +0300

    adapted header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
             to="/"
             onClick={closeMenu}
           >
-            Ruslana
+            <p className="truncate-to-first-letter">Ruslana</p>
           </NavLink>
         </div>
 
