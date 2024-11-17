d86dbcf57500266e62ce1532fbc662a2541fba2e
Matviienko Ruslana <brauniesrus@gmail.com>
2024-11-17 12:01:46 +0200

    description menu

--- src/components/DescriptionMenu/DescriptionMenu.jsx
+++ src/components/DescriptionMenu/DescriptionMenu.jsx
@@ -4,7 +4,16 @@ import style from './DescriptionMenu.module.css';
 const DescriptionMenu = () => {
   return (
     <div className={style.descriptionContainer}>
-      <p>Description in the development process</p>
+      <p className={style.descriptionTitle}>Description</p>
+      <p className={style.descriptionText}>
+        "Some elements on this portfolio site may not be essential, but this
+        page serves as a creative space to demonstrate additional skills and
+        ideas. It’s a place where I experiment with new features, designs, and
+        concepts to showcase my growth as a developer. Over time, you’ll find
+        more examples of my work here, from innovative solutions to experimental
+        projects. Thank you for taking the time to explore this page, and I hope
+        it inspires you to see the range of what I can create!"
+      </p>
     </div>
   );
 }
