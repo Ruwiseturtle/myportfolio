39f50376d21341d81827d7f0a0f120eae0baa9dd
Matviienko Ruslana <brauniesrus@gmail.com>
2024-11-19 16:27:26 +0200

    authorization

--- src/components/Authorization/Authorization.jsx
+++ src/components/Authorization/Authorization.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from "react";
 import style from './Authorization.module.css';
+import SignIn from "../SignIn/SignIn";
+import SignUp from "../SignUp/SignUp";
+
 
 const Authorization = () => {
+  const [activeTab, setActiveTab] = useState("signIn");
+
   return (
-    <div className={style.authorizationContainer}>
-      <p >Authorization in the development process</p>
-    </div>
+    <>
+      Authorization in the development process
+      <div className={style.authorizationContainer}>
+        <div className={style.tabs}>
+          <button
+            className={`${style.tab} ${
+              activeTab === "signIn" ? style.active : ""
+            }`}
+            onClick={() => setActiveTab("signIn")}
+          >
+            SIGN IN
+          </button>
+          <button
+            className={`${style.tab} ${
+              activeTab === "signUp" ? style.active : ""
+            }`}
+            onClick={() => setActiveTab("signUp")}
+          >
+            SIGN UP
+          </button>
+        </div>
+        <div className={style.content}>
+          {activeTab === "signIn" && <SignIn />}
+          {activeTab === "signUp" && <SignUp />}
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/SignIn/SignIn.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import style from "./SignIn.module.css";
+
+
+
+const SignIn = () => {
+  
+
+  return(
+  <div >
+    SignIn
+  </div>
+  )
+};
+
+export default SignIn;
