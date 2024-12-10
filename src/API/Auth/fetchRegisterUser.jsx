ee43117ee8db8eb19594f2a0db519977c080c21b
Matviienko Ruslana <brauniesrus@gmail.com>
2024-12-10 14:15:00 +0200

    token

--- src/API/Auth/fetchRegisterUser.jsx
+++ src/API/Auth/fetchRegisterUser.jsx
@@ -33,6 +33,6 @@ export const requestLogin = async formData => {
   console.log('=============login=======================');
   console.log(data);
   console.log('====================================');
-  setToken(data.token);
+  // setToken(data.token);
   return data;
 };
