337697aaec00d2834f16f0656df95b309d2f4ab4
Matviienko Ruslana <brauniesrus@gmail.com>
2025-01-24 10:04:14 +0200

    added polska language

--- src/components/LanguageMenu/LanguageMenu.jsx
+++ src/components/LanguageMenu/LanguageMenu.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './LanguageMenu.css';
 import EnIcon from "../../assets/flagsIcons/CircleFlagsEn.svg";
 import UaIcon from "../../assets/flagsIcons/CircleFlagsUa.svg";
+import PlIcon from "../../assets/flagsIcons/CircleFlagsPl.svg";
 
 // https://icones.js.org/ - іконки взяті з цього сайту
 
@@ -9,15 +10,16 @@ const LanguageMenu = ({isLangMenuOpen, setIsLangMenuOpen, changeLanguage}) => {
 
 
   return (
-    <div className={`container-lang-menu ${isLangMenuOpen ? "openLangMenu" : ""}`}>
-      <div className='box-lang-menu-list'>
+    <div
+      className={`container-lang-menu ${isLangMenuOpen ? "openLangMenu" : ""}`}
+    >
+      <div className="box-lang-menu-list">
         <div className="lang-menu-list">
+          {/* англійська мова */}
           <div
             className="iconAndLangname"
             onClick={() => {
               setIsLangMenuOpen(false);
-              console.log("=======setIsLangMenuOpen===========");
-              console.log(isLangMenuOpen);
               changeLanguage("en");
             }}
           >
@@ -28,12 +30,12 @@ const LanguageMenu = ({isLangMenuOpen, setIsLangMenuOpen, changeLanguage}) => {
             />
             <div className="langText">English</div>
           </div>
+
+          {/* українська мова */}
           <div
             className="iconAndLangname"
             onClick={() => {
               setIsLangMenuOpen(false);
-              console.log("=======setIsLangMenuOpen===========");
-              console.log(isLangMenuOpen);
               changeLanguage("ua");
             }}
           >
@@ -43,6 +45,22 @@ const LanguageMenu = ({isLangMenuOpen, setIsLangMenuOpen, changeLanguage}) => {
               alt="avatar"
             />
             <div className="langText">Українська</div>
+          </div>
+
+          {/* польска мова */}
+          <div
+            className="iconAndLangname"
+            onClick={() => {
+              setIsLangMenuOpen(false);
+              changeLanguage("pl");
+            }}
+          >
+            <img
+              className="iconLang"
+              src={PlIcon} // Зміна зображення залежно від стану меню
+              alt="avatar"
+            />
+            <div className="langText">Polska</div>
           </div>
         </div>
       </div>
--- src/i18n.js
+++ src/i18n.js
@@ -2,11 +2,13 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import languageEN from "./locales/en/translation.json"; // Файл англійської мови
 import languageUA from "./locales/ua/translation.json"; // Файл української мови
+import languagePL from "./locales/pl/translation.json"; // Файл польскої мови
 
 i18n.use(initReactI18next).init({
   resources: {
     en: { translation: languageEN },
     ua: { translation: languageUA },
+    pl: { translation: languagePL },
   },
   lng: "en", // Мова за замовчуванням
   fallbackLng: "en", // Резервна мова
